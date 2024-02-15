e3d6e5477093608dc708b36d493a7c3def70e7bd
Gautam Kumar Ray <vmggsray1997@gmail.com>
2024-02-15 14:11:56 +0530

    Project Created

---
+++ src/main/java/in/gov/forest/wildlifemis/WildlifeMisApplication.java
@@ -0,0 +1,13 @@
+package in.gov.forest.wildlifemis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildlifeMisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildlifeMisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/gov/forest/wildlifemis/WildlifeMisApplicationTests.java
@@ -0,0 +1,13 @@
+package in.gov.forest.wildlifemis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildlifeMisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
