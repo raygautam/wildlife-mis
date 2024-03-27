76bba53f5df222c362df4433d361b5257fabc45a
Gautam Kumar Ray <vmggsray1997@gmail.com>
2024-03-27 13:27:31 +0530

    Adding new field to Notification Entity.

--- src/main/java/in/gov/forest/wildlifemis/WildlifeMisApplication.java
+++ src/main/java/in/gov/forest/wildlifemis/WildlifeMisApplication.java
@@ -2,8 +2,7 @@ package in.gov.forest.wildlifemis;
 
 
 //import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
