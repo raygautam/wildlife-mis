75910a4a7fdf91cbab1a8eb772cee505eecb6903
Gautam Kumar Ray <vmggsray1997@gmail.com>
2024-04-10 17:02:44 +0530

    Add @Repository annotation.

--- src/main/java/in/gov/forest/wildlifemis/externalURL/ExternalURLRepository.java
+++ src/main/java/in/gov/forest/wildlifemis/externalURL/ExternalURLRepository.java
@@ -2,6 +2,8 @@ package in.gov.forest.wildlifemis.externalURL;
 
 import in.gov.forest.wildlifemis.domian.ExternalURL;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ExternalURLRepository extends JpaRepository<ExternalURL, Long> {
 }
